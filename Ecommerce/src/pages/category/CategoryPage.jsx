de30dd4b3ebf2b3708a44a45517e038370be985c
ADITYA SHARMA <adityasharma121415@gmail.com>
2024-07-21 21:19:45 +0530

    updated category add to cart button

--- Ecommerce/src/pages/category/CategoryPage.jsx
+++ Ecommerce/src/pages/category/CategoryPage.jsx
@@ -97,7 +97,7 @@ const CategoryPage = () => {
 
                                                                         <button
                                                                             onClick={() => addCart(item)}
-                                                                            className=" bg-pink-500 hover:bg-pink-600 w-full text-white py-[4px] rounded-lg font-bold">
+                                                                            className=" bg-black hover:bg-red-600 w-full text-white py-[4px] rounded-lg font-bold">
                                                                             Add To Cart
                                                                         </button>
                                                                     }
